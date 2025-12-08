f72b8b558e110c8b449d1e0cdf836c6d4b1f3050
Sulalitha Bowala, PhD <sulalitha.bowala@outlook.com>
2025-12-08 14:37:46 -0600

    Add files via upload

---
+++ Chapter 1 - SQL Statement Fundamentals.sql
@@ -0,0 +1,31 @@
+-- SELECT Statement
+
+-- Syntax: SELECT column name(s) FROM table name;
+
+SELECT * FROM actor;
+
+SELECT first_name FROM actor;
+
+SELECT first_name, last_name FROM actor;
+
+SELECT last_name, first_name FROM actor;
+
+-- Situation: Send a promotional email to our existing customers.
+-- We need first name, last name, and customer email of every customer (from cutomer table)
+
+SELECT * FROM customer;
+
+SELECT first_name, last_name, email FROM customer;
+
+-- SELELCT DISTINCT
+
+
+
+
+
+
+
+
+
+
+
