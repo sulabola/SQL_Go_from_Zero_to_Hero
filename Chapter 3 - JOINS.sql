ba5cc83d4add61f8fe1d41a801c6c3dca815865a
Sulalitha Bowala, PhD <sulalitha.bowala@outlook.com>
2025-12-10 15:00:55 -0600

    Add files via upload

---
+++ Chapter 3 - JOINS.sql
@@ -0,0 +1,9 @@
+-- This chapter investigates how to combine tables in out database with the use of "JOIN" statements.
+
+-- "AS" Statement
+
+-- This works as an alias for a column or result.
+
+-- Syntax:	SELECT column name AS new column name
+--			FROM table name
+
