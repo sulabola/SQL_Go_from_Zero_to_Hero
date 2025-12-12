29717aad6d8b2dfe971356852a3724bf64d0abf0
Sulalitha Bowala, PhD <sulalitha.bowala@outlook.com>
2025-12-12 15:06:23 -0600

    Add files via upload

--- Chapter 3 - JOINS.sql
+++ Chapter 3 - JOINS.sql
@@ -119,9 +119,81 @@ FROM customer;
 -- Note: Here the order of the tables matter. (All in TableA and match records in TableB)
 -- Note: If the record is only in 'TableB', it will not report with the query.
 
+-- "LEFT OUTER JOIN" with "WHERE"
 
+-- This helps to get rows unique to the left table.
 
+-- Syntax:	SELECT *
+--			FROM TableA
+--			LEFT OUTER JOIN TableB ON TableA.col_match = TableB.col_match
+--			WHERE TableB.id IS NULL;
 
+SELECT *
+FROM film;
 
+SELECT *
+FROM inventory;
 
+SELECT film.film_id, title, inventory_id, store_id
+FROM film
+LEFT JOIN inventory ON inventory.film_id = film.film_id;
 
+SELECT film.film_id, title, inventory_id, store_id
+FROM film
+LEFT JOIN inventory ON inventory.film_id = film.film_id
+WHERE inventory.film_id IS NULL;
+
+-- This will give us list if film title that we have information but not avaiable in inventory.
+
+-- "RIGHT JOIN"
+
+-- This is very similar to "LEFT JOIN", except the tables are switched.
+-- Note: We can simply switch TableA and TableB in "LEFT JOIN" and get same result.
+
+-- "UNION" Operator
+
+-- This operator is used to combine the result-set of two or more SELECT statements.
+-- That is, concatenate two results together.
+
+-- Syntax:	SELECT column_name(s) FROM table1
+--			UNION
+--			SELECT column_name(s) FROM table2
+
+-- Note: Tables should match in order to stack them.
+
+-- Note: We can also have multiple UNION statements.
+
+-- Challenge 1: What are the emails of the cutomers who live in California?
+
+SELECT *
+FROM customer;
+
+SELECT *
+FROM address;
+
+SELECT customer.email AS email_addresses
+FROM customer
+INNER JOIN address ON customer.address_id = address.address_id
+WHERE address.district = 'California';
+
+-- Challenge 2: Get a list of all the movies 'Nick Wahlberg' has been in.
+
+SELECT *
+FROM film
+LIMIT 5;
+
+SELECT *
+FROM film_actor
+LIMIT 5;
+
+SELECT *
+FROM actor
+LIMIT 5;
+
+-- Connection: film <-> film_actor <-> actor
+
+SELECT film_actor.film_id, film.title, actor.first_name, actor.last_name
+FROM actor
+INNER JOIN film_actor ON actor.actor_id = film_actor.actor_id
+INNER JOIN film ON film.film_id = film_actor.film_id
+WHERE actor.first_name = 'Nick' AND actor.last_name = 'Wahlberg';
