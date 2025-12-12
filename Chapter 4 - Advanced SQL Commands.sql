8a469bd135d39d00994936fd9248fa84f26bd328
Sulalitha Bowala, PhD <sulalitha.bowala@outlook.com>
2025-12-12 15:38:16 -0600

    Add files via upload

---
+++ Chapter 4 - Advanced SQL Commands.sql
@@ -0,0 +1,20 @@
+-- This chapter covers several important and useful topic in SQL.
+
+-- Timestamps and EXTRACT
+-- Math Functions
+-- String Functions
+-- Sub-query (query within a query)
+-- Self-Join
+
+-- Timestamps and EXTRACT
+
+-- First we focus on displaying current time information.
+-- These are more usefull when we are creating own tables and databases.
+
+-- There are different subsets of datatypes that hold date and time information:
+
+-- TIME: Contain only time
+-- DATE: Contain only date
+-- TIMESTAMP: Contain date and time
+-- TIMESTAMPTS: Contain date, time, and timezone
+
