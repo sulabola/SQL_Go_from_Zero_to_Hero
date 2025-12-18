9805c6f7d959daa199153810cee109d0dc7aaa14
Sulalitha Bowala, PhD <sulalitha.bowala@outlook.com>
2025-12-18 14:27:04 -0600

    Add files via upload

--- Chapter 5 - Creating Databases and Tables.sql
+++ Chapter 5 - Creating Databases and Tables.sql
@@ -403,3 +403,25 @@ DROP COLUMN IF EXISTS people;
 --					age SMALLINT CHECK (age > 21),
 --					parent_age SMALLINT CHECK (parent_age > age)
 --					);
+
+CREATE TABLE employees(
+	emp_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	birthdate DATE CHECK (birthdate > '1900-01-01'),
+	hire_date DATE CHECK (hire_date > birthdate),
+	salary INTEGER CHECK (salary > 0)
+);
+
+-- Let's Insert some data to employees table
+
+INSERT INTO employees(first_name, last_name, birthdate, hire_date, salary)
+VALUES
+	('Sula', 'Bola', '1990-11-03', '2010-01-01', 100),
+	('Sammy', 'Smith', '1990-11-03', '2010-01-01', 100);
+
+SELECT *
+FROM employees;
+
+-- Note: SERIAL kept the count of failure attemps (birthdate: 1899-01-01, salary: -100)
+-- emp_id are not consecutive.
