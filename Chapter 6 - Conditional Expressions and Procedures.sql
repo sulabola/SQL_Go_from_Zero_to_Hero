4500c3431476a2f1f336ba79d9fa246ed124354a
Sulalitha Bowala, PhD <sulalitha.bowala@outlook.com>
2025-12-18 15:24:27 -0600

    Add files via upload

---
+++ Chapter 6 - Conditional Expressions and Procedures.sql
@@ -0,0 +1,25 @@
+-- This chapter covers Conditional Expressions and Procedures
+
+-- Here we focus on applying logical operators and functionality within the SQL code.
+-- We will learn key words: CASE, COALESCE, NULLIF, CASR, Views, Import and Export functionality
+
+-- "CASE" Statement
+
+-- We can use the CASE statement to only execute SQL code when certain conditions are met.
+-- Thus, it is similar to IF/ELSE statements in other programming languages.
+
+-- There are two ways to use this:
+-- 1. General CASE statement
+-- 2. CASE expression
+
+-- Note: Both methods may lead to same results.
+
+-- General syntax:	CASE
+--						WHEN condition1 THEN result1
+--						WHEN condition2 THEN result2
+--						ELSE some_other_result
+--					END
+
+
+
+
