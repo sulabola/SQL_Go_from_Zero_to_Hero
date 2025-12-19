c6a0061eb089a1975fbe7946560f812cca3648c4
Sulalitha Bowala, PhD <sulalitha.bowala@outlook.com>
2025-12-19 17:08:01 -0600

    Add files via upload

--- Chapter 6 - Conditional Expressions and Procedures.sql
+++ Chapter 6 - Conditional Expressions and Procedures.sql
@@ -172,6 +172,163 @@ FROM film;
 --	SELECT item, (price - COALESCE(discount,0)) AS final
 --	FROM table
 
+-- Note: This function is usefull when we have to perform oprations with null values. Not to change them.
+
+-- "CAST" Operator and Function
+
+-- This allows us to convert one data type inot another.
+-- However, this does not work always. It should be reasonable to convert the data.
+-- Example: String 5 to number 5 works. String five to number 5 will not work.
+
+-- There are two ways to perform this:
+
+-- 1. CAST function:
+-- SELECT CAST('5' AS INTEGER)
+
+-- 2. CAST operator: (In PorstgreSQL ONLY)
+-- SELECT '5'::INTEGER
+
+-- Note: In most cases we will be doing this for a column, not a single value.
+
+SELECT CAST('5' AS INTEGER) AS new_int;
+
+SELECT '5'::INTEGER;
+
+-- Task: What is the character length (number of digits) of inventory_id
+
+SELECT *
+FROM rental;
+
+SELECT CHAR_LENGTH(CAST(inventory_id AS VARCHAR))
+FROM rental;
+
+-- "NULLIF"
+
+-- This takes in 2 inputs and returns NULL if both are equal, otherwise it returns the first argument passed.
+
+-- Example:
+-- NULLIF(10,10);
+-- This retunrs NULL
+
+-- NULLIF(10,12);
+-- This returns 10
+
+-- This is useful in cases where a NULL value would cause an error or unwanted result.
+
+-- We test this with a new table created in a seperate database ('testme').
+
+CREATE TABLE depts(
+	first_name VARCHAR(50),
+	department VARCHAR(50)
+);
+
+INSERT INTO depts(first_name, department)
+VALUES
+	('Vinton','A'),
+	('Lauren','A'),
+	('Claire','B');
+
+SELECT *
+FROM depts;
+
+-- Task: Calculate male and female members.
+
+SELECT 
+	(SUM(CASE 
+			WHEN department = 'A' THEN 1
+			ELSE 0
+		END))/
+	(SUM(CASE
+			WHEN department = 'B' THEN 1
+			ELSE 0
+		END)) AS department_ratio
+FROM depts;
+
+-- Here we should get 2 as there are two 'A'.
+
+-- Let's see what happen when there is nobody in department B.
+
+DELETE FROM depts
+WHERE department = 'B';
+
+SELECT *
+FROM depts;
+
+-- Now if we try the above code, we will get an ERROR as we are deviding a number by zero.
+-- We can use NULLIF to fix this.
+
+SELECT 
+	(SUM(CASE 
+			WHEN department = 'A' THEN 1
+			ELSE 0
+		END))/
+	NULLIF(	
+	(SUM(CASE
+			WHEN department = 'B' THEN 1
+			ELSE 0
+		END)),0) AS department_ratio
+FROM depts;
+
+-- "VIEWS"
+
+-- Oftern there are specific combinations of tables and conditions that we find useful.
+-- Instead of having to perform the same query over and over again,
+-- we can create a VIEW to see this query with a simple call.
+
+-- Thus, a view is a database object that is of a stored query.
+-- A view can be accessed as a virtual table in PostgreSQL.
+-- Note that a view does not store data, it simply stores the query.
+
+-- We can also update andalter existing views.
+
+-- Task: Let's assume we constantly need to check customer names and their addresses.
+
+SELECT *
+FROM customer;
+
+SELECT *
+FROM address;
+
+-- In customer table we have address_id, and in address table we have addresses.
+
+SELECT first_name, last_name, address
+FROM  customer
+INNER JOIN address ON customer.address_id = address.address_id;
+
+-- Let's save this query for future use.
+
+CREATE VIEW customer_info AS
+SELECT first_name, last_name, address
+FROM  customer
+INNER JOIN address ON customer.address_id = address.address_id;
+
+-- Now:
+
+SELECT *
+FROM customer_info;
+
+-- Let's modify this query. We will use CREATE or REPLACE commands.
+
+CREATE OR REPLACE VIEW customer_info AS
+SELECT first_name, last_name, address, district
+FROM  customer
+INNER JOIN address ON customer.address_id = address.address_id;
+
+SELECT *
+FROM customer_info;
+
+-- We can also chnage the name of a view.
+
+ALTER VIEW customer_info RENAME TO c_info;
+
+SELECT *
+FROM c_info;
+
+-- We can also remove a view. It is similar to delete a table.
+
+DROP VIEW IF EXISTS c_info;
+
+
 
 
 
